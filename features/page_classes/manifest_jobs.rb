2e2aa61ea5c4109dafb49ccf85afffdf121c7954
Bob Tishma <tishma.1@osu.edu>
2016-11-28 17:00:55 -0500

    hotfix typo

--- features/page_classes/manifest_jobs.rb
+++ features/page_classes/manifest_jobs.rb
@@ -5,14 +5,14 @@ class ManifestJobs
   include PageObject
 
   divs(:location_results, class: 'opp-loc')
-  divs(:type_results, class: 'opp-loc')
+  divs(:type_results, class: 'opp-type')
 
   def get_locations
-    return location_results
+    return location_results_elements
   end
 
   def get_types
-    return type_results
+    return type_results_elements
   end
 
 end
